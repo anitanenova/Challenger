e359f2ef1b90b6ea7e1239a05035c912815cd15b
[anitanenova] <anita.nenova8@gmail.com>
2021-06-07 12:11:49 +0300

    fixed some styles on mobile devices

--- src/scripts/vendor/sample-library2.js
+++ src/scripts/vendor/sample-library2.js
@@ -1,20 +1,14 @@
 const mobileMenu = document.querySelector(".menu-mobile");
-const nav = document.querySelector(
-  "div.site-header-secondary > div > div > div > div > nav.site-navigation-primary.site-navigation-primary"
-);
+const nav = document.querySelector(".site-navigation");
 
 mobileMenu.addEventListener("click", function (e) {
   let current = e.target.classList;
 
   if (current.contains("menu-open")) {
     current.remove("menu-open");
-    // nav.style.display = "none";
-    // nav.style.transition = "0.5s";
     nav.style.transform = "translateX(-100%)";
   } else {
     current.add("menu-open");
-    // nav.style.display = "block";
-    //nav.style.transition = "0.5s";
     nav.style.transform = "translateX(0)";
   }
 });
